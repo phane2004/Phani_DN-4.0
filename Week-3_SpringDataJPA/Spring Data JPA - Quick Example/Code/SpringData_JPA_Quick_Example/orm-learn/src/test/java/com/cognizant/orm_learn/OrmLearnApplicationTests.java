a90be7a95a003e92a6a06ec586ec314966f2cd8b
PHANI <dereddyphane@gmail.com>
2025-07-06 17:40:35 +0530

    Add files via upload

---
+++ Week-3_SpringDataJPA/Spring Data JPA - Quick Example/Code/SpringData_JPA_Quick_Example/orm-learn/src/test/java/com/cognizant/orm_learn/OrmLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.orm_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//import completed
+@SpringBootTest
+class OrmLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
